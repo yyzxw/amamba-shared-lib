277502d8c165968609d8a797ab964e69222d4d16
xiaowu.zhu <xiaowu.zhu@daocloud.io>
2024-04-11 15:42:43 +0800

    add docker push

--- vars/DeployApplication.groovy
+++ vars/DeployApplication.groovy
@@ -2,7 +2,7 @@ def call(String credentialsId,String cluster, String namespace, String filepath)
     echo "deploying ${filepath} to ${cluster} in ${namespace} namespace use ${credentialsId} credentials"
     withCredentials([file(credentialsId: credentialsId, variable: 'KUBECONFIG')]) {
         echo "credentials file is ${KUBECONFIG}"
-        sh 'kubectl apply - f ${filepath} -n ${namespace}'
+        sh "kubectl --kubeconfig=${KUBECONFIG} apply -f ${filepath} --namespace=${namespace}"
     }
 }
 
