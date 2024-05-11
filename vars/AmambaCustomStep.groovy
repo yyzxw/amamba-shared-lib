6469c41160dbc421552969b99689dc65c1fc3c61
xiaowu.zhu <xiaowu.zhu@daocloud.io>
2024-05-11 11:00:37 +0800

    feat: add step

--- vars/AmambaCustomStep.groovy
+++ vars/AmambaCustomStep.groovy
@@ -1,14 +1,23 @@
-def call(Map config){
+def call(Map config) {
+    runArgs = "--entrypoint=''"
+
     String pluginID = config.pluginID
     Map args = config.args
+    string volume = config.volume
 
-    echo "Running plugin ${pluginID} with ${args}"
+    echo "Running plugin ${pluginID} with ${args}, volume: ${volume}"
+
     script {
-        withEnv(args) {
-            def image = args['image']
-            def shell = args['shell']
-            def script = args['script']
-            docker.image("$image").inside("--entrypoint=''") {
+        Map envVars = args.collectEntries { k, v ->
+            [k, v]
+        }
+        echo "envVars: ${envVars}"
+
+        withEnv(envVars) {
+            if (volume) {
+                runArgs = runArgs + " -v ${volume}"
+            }
+            docker.image("$image").inside("$runArgs") {
                 sh "$shell $script"
             }
         }
