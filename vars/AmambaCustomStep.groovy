eb600f3e5b82a424830eb20fcdecdd4d7d367eba
xiaowu.zhu <xiaowu.zhu@daocloud.io>
2024-05-11 11:34:17 +0800

    feat: add step

--- vars/AmambaCustomStep.groovy
+++ vars/AmambaCustomStep.groovy
@@ -1,24 +1,43 @@
 def call(Map config) {
+    validate(config)
     runArgs = "--entrypoint='' "
 
     String pluginID = config.pluginID
-    echo "run amamba custom step ${pluginID}"
-
     Map args = config.args
-    echo "plugin args ${args}"
+    echo "run amamba custom plugin ${pluginID} with args ${args}"
 
     script {
         String envVars = args.collect { k, v ->
             "-e ${k}=${v}"
         }.join(' ')
-
         echo "inject env vars ${envVars}"
 
         runArgs = runArgs + " ${envVars}"
+        //TODO add -v for each volume
         docker.image(args.image).inside("$runArgs") {
-            sh "$args.shell $args.script"
+            if (args.shell) {
+                sh "$args.shell $args.script"
+            } else {
+                sh "$args.script"
+            }
         }
     }
+}
+
+def validate(Map config) {
+    if (!config.pluginID) {
+        error "pluginID is required"
+    }
+    if (!config.args) {
+        error "args is required"
+    }
+    if (!config.args.image) {
+        error "args.image is required"
+    }
+    if (!config.args.script) {
+        error "args.script is required"
+    }
+    return this
 }
 
 return this
