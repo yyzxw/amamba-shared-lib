020f4090a553209c24a5c892b947f94d285696d3
xiaowu.zhu <xiaowu.zhu@daocloud.io>
2024-05-11 10:35:47 +0800

    feat: add step

--- vars/AmambaCustomStep.groovy
+++ vars/AmambaCustomStep.groovy
@@ -1,18 +1,9 @@
-def call(Map args){
-    def envs = [:]
-    args.each { k, v ->
-        envs[k] = v
-    }
-
-    def image = envs['image']
-    def shell = envs['shell']
-    def script = envs['script']
-    def pluginID = envs['pluginID']
-    if (image == null || shell == null || script == null || pluginID == null) {
-        error("Missing required parameters")
-    }
-    echo "Running plugin ${pluginID} with ${envs}"
-    withEnv(envs) {
+def call(String pluginID,Map args){
+    echo "Running plugin ${pluginID} with ${args}"
+    withEnv(args) {
+        def image = args['image']
+        def shell = args['shell']
+        def script = args['script']
         docker.image("$image").inside("--entrypoint=''") {
             sh "$shell $script"
         }
